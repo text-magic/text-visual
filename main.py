5d1b50d1eb577ed14d28785bf957ecad8ce03fd3
Zhi.Huang <zh.impact@outlook.com>
2025-08-22 15:48:34 +0800

    feat: add FastAPI backend with HanLP tokenization endpoint

---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+import hanlp
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+HanLP = hanlp.load(hanlp.pretrained.mtl.UD_ONTONOTES_TOK_POS_LEM_FEA_NER_SRL_DEP_SDP_CON_XLMR_BASE)
+
+
+app = FastAPI()
+
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/tok")
+def tok(text: str):
+    result = HanLP(text, tasks="tok")
+    return {"result": result}
