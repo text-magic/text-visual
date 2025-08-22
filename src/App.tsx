876e91e4a96f6f929c6f09f5c5c9e22f41724bc6
Zhi.Huang <zh.impact@outlook.com>
2025-08-22 15:48:42 +0800

    feat: add text tokenization preview with API integration and update editor config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [text, setText] = useState("");
 
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    if (!text) return;
+    fetch("http://localhost:8000/tok?text=" + text)
+      .then((response) => response.json())
+      .then((data) => setResult(data));
+  }, [text]);
+
   return (
-    <>
-      <div>
-        <h1>Text Visual</h1>
-        <textarea
-          name="text"
-          id=""
-          rows={10}
-          cols={50}
-          value={text}
-          style={{ width: "100%", height: "100%" }}
-          onChange={(e) => setText(e.target.value)}
-        ></textarea>
-      </div>
-    </>
+    <div>
+      <h1>Text Visual</h1>
+      <textarea
+        name="text"
+        id=""
+        rows={10}
+        cols={50}
+        value={text}
+        style={{ width: "100%", height: "100%" }}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+
+      <div>{result?.tok?.map((item: any) => item.word).join(" ")}</div>
+    </div>
   );
 }
 
